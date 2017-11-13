d9b221b904fe34f5fb789fc183606145fbcf580f
Tal Weinfeld <talw@webwise.co.il>
2017-11-13 11:45:04 +0200

    Refactored

--- server.js
+++ server.js
@@ -2,15 +2,39 @@ const
     _ = require('lodash'),
     path = require('path'),
     kefir = require('kefir'),
-    express = require('express');
+    express = require('express'),
+    Kubemote = require('kubemote');
 
-let app = express();
+let
+    kubeClient = new Kubemote(Kubemote.CONFIGURATION_FILE()),
+    app = express();
 
-app.get('/log', (req, res)=>{
+/*let rawLogStream = kefir
+    .stream(({ emit })=> {
+        let destroy = _.noop;
+        kubeClient.watchDeploymentList().then((func)=>{
+            destroy = func;
+            emit();
+        });
+        return ()=> destroy();
+    })
+    .flatMap(()=> kefir.fromEvents(kubeClient, 'watch').map((event)=> ({ time: Date.now(), event })));
+
+let logCacheProperty = rawLogStream.scan(_.concat);
+
+    
+kefir.combine(
+    [ kefir.stream(({ emit })=> app.get('/log', (req, res)=> emit({ req, res }))).toProperty() ],
+    [ kefir.concat(rawLogStream.scan(_.concat))logProperty ]
+).onValue(([{ req, res }, log])=> {
+    res.send();
+});*/
+
+app.get('/log', (req, res)=> {
     res.sendFile(path.join(__dirname, './data/dump.jsonl'));
 });
 app.use(express.static('public'));
 app.use('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, './flat_list.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 app.listen(8080);
