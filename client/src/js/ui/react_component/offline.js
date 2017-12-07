c92dec65b8b656659634e67551a5b1c574d4e501
Tal Weinfeld <talw@webwise.co.il>
2017-12-07 12:20:48 +0200

    typo (again)

--- client/src/js/ui/react_component/offline.js
+++ client/src/js/ui/react_component/offline.js
@@ -6,6 +6,6 @@ const [div, h1, p] = ["div", "h1", "p"].map((elementName)=> _.partial(React.crea
 export default function(){
     return div({ className: "offline" }, [], ...[
         h1({}, "0011011001011010001001010  10   0    1     0       1"),
-        ..."aw-shucks! ..it's looks like we've lost connection to the server.\nstand by. no need to refresh, we'll resume as soon we regain connection.".split('\n').map(_.unary(_.partial(p, {})))
+        ..."aw-shucks! ..it's looks like we've lost connection to the server.\nstand by. no need to refresh, we'll resume as soon as we regain connection.".split('\n').map(_.unary(_.partial(p, {})))
     ]);
 };
